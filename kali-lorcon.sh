1750fe2df5a67bca8743e4829ca17491878ea023
0x90 <oleg.kupreev@gmail.com>
2014-04-02 03:42:14 +0400

    + lorcon

--- kali-lorcon.sh
+++ kali-lorcon.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-
+echo "Installing Lorcon dependecies"
 apt-get install libpcap0.8-dev libnl-dev
 
 #Requires lorcon2:
+echo "Installing Lorcon"
 cd /usr/src
 git clone https://code.google.com/p/lorcon
 cd lorcon
@@ -12,6 +13,7 @@ make
 make install
 
 # install pylorcon
+echo "Install pylorcon2"
 cd /usr/src
 svn checkout http://pylorcon2.googlecode.com/svn/trunk/ pylorcon2
 cd pylorcon2
