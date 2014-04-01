61f5be066d8ec62a4d36b54eba0667165490fe26
0x90 <oleg.kupreev@gmail.com>
2014-04-02 03:49:15 +0400

    + lorcon

--- kali-lorcon.sh
+++ kali-lorcon.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Latest version of Pylorcon2 https://github.com/tom5760/pylorcon2
+#
 
 echo "Installing Lorcon dependecies"
 apt-get install libpcap0.8-dev libnl-dev
@@ -12,7 +14,16 @@ cd lorcon
 make
 make install
 
-# install pylorcon
+# install pylorcon. Deprecated?
+#echo "Install pylorcon"
+#cd /usr/src
+#svn checkout http://pylorcon.googlecode.com/svn/trunk/ pylorcon
+#cd pylorcon
+#python setup.py build
+#python setup.py install
+
+
+# install pylorcon2
 echo "Install pylorcon2"
 cd /usr/src
 svn checkout http://pylorcon2.googlecode.com/svn/trunk/ pylorcon2
