d09cb41086003afe90ca3c293d66343105826b6b
0x90 <oleg.kupreev@gmail.com>
2014-04-02 03:40:03 +0400

    + lorcon

---
+++ kali-lorcon.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+apt-get install libpcap0.8-dev libnl-dev
+
+#Requires lorcon2:
+cd /usr/src
+git clone https://code.google.com/p/lorcon
+cd lorcon
+./configure
+make
+make install
+
+# install pylorcon
+cd /usr/src
+svn checkout http://pylorcon2.googlecode.com/svn/trunk/ pylorcon2
+cd pylorcon2
+python setup.py build
+python setup.py install
+
+# to make lorcon available to metasploit
+#cd ../ruby-lorcon/
+#ruby extconf.rb
+#make
+#make install
