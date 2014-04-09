f833dfe36868bf1024336009e0b2de60fd178dac
0x90 <oleg.kupreev@gmail.com>
2014-04-10 01:00:14 +0400

    separate folder for desktop scripts

---
+++ desktop/kali-kde.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+. helper.sh
+
+install_kde(){
+# How to install KDE Plasma Desktop Environment in Kali Linux:
+apt-get install kali-defaults kali-root-login desktop-base kde-plasma-desktop
+
+# How to install Netbook KDE Plasma Desktop Environment in Kali Linux:
+apt-get install kali-defaults kali-root-login desktop-base kde-plasma-netbook
+
+# How to install Standard Debian selected packages and frameworks in Kali Linux:
+apt-get install kali-defaults kali-root-login desktop-base kde-standard
+
+# How to install KDE Full Install in Kali Linux:
+apt-get install kali-defaults kali-root-login desktop-base kde-full
+
+# How to remove KDE on Kali Linux:
+apt-get remove kde-plasma-desktop kde-plasma-netbook kde-standard
+}
