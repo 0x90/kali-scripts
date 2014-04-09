b54ba4e02a36341c946c39b7df2c35f683d699ce
0x90 <oleg.kupreev@gmail.com>
2014-04-10 01:14:12 +0400

    + mobile folder + python folder

--- kali-live-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#OR BUILD YOUR OWN CUSTOM ISO.
-apt-get install git live-build cdebootstrap
-git clone git://git.kali.org/live-build-config.git
-cd live-build-config
-lb config
-lb build
-
--- kali-scapytain.sh
+++
@@ -1,7 +0,0 @@
-apt-get install -y python-cherrypy3 graphviz python-genshi python-sqlobject python-formencode python-pyopenssl highlight python-trml2pdf python-pip
-pip install pyopenssl
-
-mcedit /etc/scapytainrc
-
-mkdir /var/lib/scapytain
-scapytain_dbutil -c
