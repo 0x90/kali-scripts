81353242f0ed4998b2a1dd71b1f05d92512ab352
0x90 <g17pwd1488>
2014-04-12 08:33:42 +0400

    + kali-bluetooth.sh

--- kali-bluetooth.sh
+++ kali-bluetooth.sh
@@ -3,7 +3,7 @@
 
 install_bluetooth(){
     print_status "Installing dependencies for BlueMaho"
-    apt-get install anyremote redfang spooftooph python-bluez  kismet-plugins obexfs ubertooth bluepot bluewho btscanner
+    apt-get install anyremote redfang spooftooph python-bluez  kismet-plugins obexfs ubertooth bluepot bluewho btscanner bluez-utils
     apt-get install libopenobex1:i386 libopenobex1-dev:i386 libbluetooth-dev:i386
 
     wget "https://wiki.thc.org/BlueMaho?action=AttachFile&do=get&target=bluemaho_v090417.tgz"
