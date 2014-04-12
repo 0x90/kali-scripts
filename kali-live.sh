71e3f86e70a37c1b83c339e497206d558242c96f
0x90 <oleg.kupreev@gmail.com>
2014-04-12 05:03:41 +0400

    * kali-live

--- kali-live.sh
+++ kali-live.sh
@@ -73,3 +73,15 @@ mkdir /mnt/usb
 mount /dev/sdb2 /mnt/usb
 echo "/ union" >> /mnt/usb/persistence.conf
 umount /mnt/usb
+
+
+
+
+# Fetch the latest Kali debootstrap script from git
+curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=HEAD" > kali-debootstrap &&\
+sudo debootstrap kali
+./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
+sudo tar -C kali-root -c . | sudo docker import - linux/kali &&\
+rm -rf ./kali-root &&\
+docker run -t -i linux/kali cat /etc/debian_version &&\
+echo "Build OK" || echo "Build failed!"
