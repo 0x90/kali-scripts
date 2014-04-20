4b2fa02070496e7887a3053f18de11adbc172cbf
0x90 <oleg.kupreev@gmail.com>
2014-04-20 05:13:26 +0400

    ! kali-dev bugfix

--- kali-dev.sh
+++ kali-dev.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 . helper.sh
 
-
-
 install_devel(){
 
     print_status "Installing development tools and environment"
@@ -31,29 +29,3 @@ install_devel(){
 }
 
 install_devel
-
-
-##!/bin/bash
-#. helper.sh
-#
-#apt-get install gcc-arm-linux-gnueabi libc6-dev-armel-cross qemu git-core gnupg flex bison gperf \
-# libesd0-dev build-essential zip curl libncurses5-dev zlib1g-dev libncurses5-dev gcc-multilib g++-multilib
-#
-#
-#if [ `getconf LONG_BIT` = "64" ]
-#then
-#    echo "64-bit OS detected install 32-bit libs"
-#    dpkg --add-architecture i386
-#    apt-get update -y
-#    apt-get install ia32-libs -y
-#fi
-#
-## Скачайте Linaro кросс-компилятор из нашего Git репозитория.
-#cd ~
-#mkdir -p arm-stuff/kernel/toolchains
-#cd arm-stuff/kernel/toolchains
-#git clone git://github.com/offensive-security/arm-eabi-linaro-4.6.2.git
-#
-#echo "USE following"
-#echo "export ARCH=arm"
-#echo "export CROSS_COMPILE=~/arm-stuff/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-linaro-4.6.2"
--- kali-embedded.sh
+++ kali-embedded.sh
@@ -113,3 +113,30 @@ install_embedded(){
 }
 
 install_embedded
+
+
+
+##!/bin/bash
+#. helper.sh
+#
+#apt-get install gcc-arm-linux-gnueabi libc6-dev-armel-cross qemu git-core gnupg flex bison gperf \
+# libesd0-dev build-essential zip curl libncurses5-dev zlib1g-dev libncurses5-dev gcc-multilib g++-multilib
+#
+#
+#if [ `getconf LONG_BIT` = "64" ]
+#then
+#    echo "64-bit OS detected install 32-bit libs"
+#    dpkg --add-architecture i386
+#    apt-get update -y
+#    apt-get install ia32-libs -y
+#fi
+#
+## Скачайте Linaro кросс-компилятор из нашего Git репозитория.
+#cd ~
+#mkdir -p arm-stuff/kernel/toolchains
+#cd arm-stuff/kernel/toolchains
+#git clone git://github.com/offensive-security/arm-eabi-linaro-4.6.2.git
+#
+#echo "USE following"
+#echo "export ARCH=arm"
+#echo "export CROSS_COMPILE=~/arm-stuff/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-linaro-4.6.2"
