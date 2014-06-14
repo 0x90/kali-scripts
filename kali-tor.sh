fa0e7358b019680b88fcd5e99f5da46129da64c9
0x90 <oleg.kupreev@gmail.com>
2014-06-15 01:31:14 +0400

    + tor install

---
+++ kali-tor.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+apt-get install vidalia privoxy
+
+echo forward-socks4a / 127.0.0.1:9050 . >> /etc/privoxy/config
+
+mkdir -p /var/run/tor
+chown debian-tor:debian-tor /var/run/tor
+chmod 02750 /var/run/tor
+
+/etc/init.d/tor start
+/etc/init.d/provoxy start
+
+#USE SOCKS PROXY 127.0.0.1:9059
