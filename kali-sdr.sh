926d1c5e2d119c6c9125157d5b221ff38f31d782
090h <oleg.kupreev@gmail.com>
2014-08-10 08:54:44 +0400

    Create kali-sdr.sh

---
+++ kali-sdr.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+. helper.sh
+
+### RTFM
+# http://labs.inguardians.com/
+# http://nutaq.com/en/blog/complete-model-based-pc-fpga-sdr-development-environment-gnu-radio
+# http://funoverip.net/2014/07/gnu-radio-cc1111-packets-encoderdecoder-blocks/
+
+## BladeRF
+# https://github.com/Nuand/bladeRF
+# https://github.com/jmichelp/sdrsharp-bladerf
+
+
+### DVB
+# https://github.com/drmpeg/gr-dvbs
+# https://github.com/BogdanDIA/gr-dvbt
+# https://github.com/drmpeg/gr-dvbs2
+
+###
+# https://github.com/argilo/gr-dsd
+
+### GPS
+# https://github.com/davidhodo/gnuradio_gps
+
+### 802.1X
+# https://github.com/bastibl/gr-ieee802-11
+# https://github.com/bastibl/gr-ieee802-15-4
+# https://github.com/wishi/gr_802.15.4
+# https://github.com/UpYou/gr-ieee802-15-5
+# https://github.com/septikus/gnuradio-802.15.4-demodulation
+
+
+### MOBILE NETWORKS ###
+# git://git.osmocom.org/gr-osmosdr)
+# https://github.com/anastas/gr-cdma
+# https://github.com/ptrkrysik/gr-gsm
+# https://github.com/scateu/airprobe-3.7-hackrf-patch
+# https://github.com/oWCTejLVlFyNztcBnOoh/gr-lte
+# https://github.com/NightlyDev/Airprobe-GSM-RCV-GR3.7
+# https://github.com/neo4reo/OpenLTE
+# http://sourceforge.net/projects/openlte/
+# https://github.com/kit-cel/gr-lte
+# https://github.com/libLTE/libLTE
+# https://github.com/Evrytania/LTE-Cell-Scanner
+# https://github.com/JiaoXianjun/rtl-sdr-LTE
+# https://github.com/snailpy/ltetoolkit
+# https://github.com/tinkerbeast/understanding-lte-in-python
+
+# Code for using GNURadio w/ a USRP
+# https://github.com/edwatt/REU2014
+
+##### Blocks
+# https://github.com/guruofquality/grex
+# https://github.com/guruofquality/grextras
+# https://github.com/bistromath/gr-ais
+# https://github.com/dl1ksv/gr-display
+# https://github.com/fewu/gnuradio_drm
+# https://github.com/argilo/gr-qam
+# https://github.com/osh/gr-bitcoin
+# https://github.com/bistromath/gr-air-modes
+# https://github.com/mogar/uhd_ofdm
+# https://github.com/glneo/gr-clicker
+# https://github.com/scateu/gr-remotecar
+# https://github.com/dl1ksv/gr-ax25
+# https://github.com/bastibl/gr-rds
+# https://github.com/argilo/gr-ham
+
+#### Misc
+# https://github.com/azorg/gnuradio-examples
+# https://github.com/UpYou/gnuradio-tools
+# https://github.com/bistromath/gr-scanner
+# https://github.com/n-west/gr-west
+# https://github.com/urbank/beacon
+# https://github.com/piratfm/gr-ssrptx
+# https://github.com/argilo/sdr-examples
+# https://github.com/argilo/ham-utils
