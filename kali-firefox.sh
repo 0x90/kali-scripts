bb561945184122e8c2883210fc509daa0bd9f4bb
0x90 <z3r0trust!>
2015-01-15 18:30:27 +0300

    +kali-firefox

---
+++ kali-firefox.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+apt-get remove iceweasel
+echo -e "\ndeb http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt all main" | tee -a /etc/apt/sources.list > /dev/null
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C1289A29
+apt-get update
+apt-get install firefox-mozilla-build
