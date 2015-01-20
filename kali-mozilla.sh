211988e928e3af73c9b765559c9860d4de662ef6
root <root@localhost.localdomain>
2015-01-20 21:32:06 +0300

    mozilla install buhgfix

--- kali-mozilla.sh
+++ kali-mozilla.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get remove iceweasel
-echo -e "\ndeb http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt all main" | tee -a /etc/apt/sources.list > /dev/null
+echo "deb http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt all main" >> /etc/apt/sources.list 
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C1289A29
 apt-get update
 apt-get install firefox-mozilla-build
