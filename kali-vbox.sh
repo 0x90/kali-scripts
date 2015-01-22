9c62690561ea081a2866f89daa59374c409c55f5
0x90 <oleg.kupreev@gmail.com>
2015-01-22 14:13:17 +0300

    +kali-vbox.sh

---
+++ kali-vbox.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Add VB repo"
+echo >> /etc/apt/sources.list
+echo "deb http://download.virtualbox.org/virtualbox/debian wheezy contrib" >> /etc/apt/sources.list
+
+echo "Add VB key"
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+apt-get update -y
+apt-get install dkms -y
+apt-get install virtualbox-4.3 -y
+
