848fdbaa69d6e460b3944e4daa12cd5bff9cb73e
Oleg Kupreev <oleg.kupreev@gmail.com>
2020-01-03 19:04:22 +0300

    added shell.sh

---
+++ shell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# install zsh+prezto+powerline
+
+# zsh
+apt install zsh fontconfig rake -y
+
+# screen
+apt install screen tmux -y
+
+# https://github.com/skwp/dotfiles
+sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh `"
+
